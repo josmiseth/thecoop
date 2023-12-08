038dab66633cbca14e3d78b6ecf07eb75d9d66d1
Jo Smiseth <josmiseth@gmail.com>
2023-12-08 22:34:52 +0100

    add wiring description and new pin names

--- src/open-hatch.py
+++ src/open-hatch.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
+
+'''
+Relay wiring
+
+Relay channel     Wire color     RB Port     Function
+    4             Yellow         GPIO17
+    3             Green          GPIO05
+    2             Dark blue      GPIO06
+    1             Blue           GPIO07     
+
+'''
+
 import time
 from time import sleep
 import RPi.GPIO as GPIO
 
+
 print("Open hatch")
-relay_pluss_up = 17
-relay_minus_up = 5
+relay_minus_up = 17
+relay_plus_up = 5
 time_to_run = 5.0
-state_file_name = "/tmp/thecoop/hatch_status.text"
+state_file_name = "/tmp/thecoop/hatch_status.txt"
 
 def set_hatch_status(status, filename):
     
@@ -16,13 +29,13 @@ def set_hatch_status(status, filename):
         file.write(status)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(relay_pluss_up, GPIO.OUT)
+GPIO.setup(relay_plus_up, GPIO.OUT)
 GPIO.setup(relay_minus_up, GPIO.OUT)
 
 #Save hatch status running to file
 set_hatch_status("in_motion", state_file_name)
 
-GPIO.output(relay_pluss_up, GPIO.LOW)
+GPIO.output(relay_plus_up, GPIO.LOW)
 GPIO.output(relay_minus_up, GPIO.LOW)
 
 time.sleep(time_to_run)
@@ -35,4 +48,4 @@ GPIO.cleanup()
 set_hatch_status("open", state_file_name)
 
 print("Open hatch finished")
-
+exit()
