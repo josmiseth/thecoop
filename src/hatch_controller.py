2b0e9b419955a073b2c6bc934448580874a07b5a
Jo Smiseth <josmiseth@gmail.com>
2023-12-20 21:22:46 +0100

    change cron time

--- src/hatch_controller.py
+++ src/hatch_controller.py
@@ -163,8 +163,8 @@ def start_controller():
 
 
     logger.info("Adding cron job")
-    sched.add_job(open_hatch, 'cron', hour=21, minute=17)
-    sched.add_job(close_hatch, 'cron', hour=21, minute=18)
+    sched.add_job(open_hatch, 'cron', hour=21, minute=23)
+    sched.add_job(close_hatch, 'cron', hour=21, minute=24)
 
     sched.print_jobs()
     
