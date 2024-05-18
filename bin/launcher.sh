655feb93123ffa9bc47a9fffb3a82398b9db9384
Jo Smiseth <josmiseth@gmail.com>
2024-05-18 22:18:32 +0200

    Add luancher.sh script to launch from crontab on reboot

---
+++ bin/launcher.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+# launcher.sh
+# navigate to home directory, then to the source directory, then execute the python script, then back to home
+cd /
+cd home/josmi/projects/thecoop/src
+sudo python hatch_controller.py
+cd /
