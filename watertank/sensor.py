067bfe2633acd43eed671caca155fd2db19dfa55
Jo <josmiseth@gmail.com>
2024-06-15 09:08:34 +0200

    add tibber pushnotification query

--- watertank/sensor.py
+++ watertank/sensor.py
@@ -87,7 +87,47 @@ def low_level_warning(dist):
                       time.sleep(1)                      
 	else:
 		print("level ok")
-		
+
+  # Welcome to the Tibber GraphQL Explorer
+  #
+  # GraphiQL is an in-browser tool for writing, validating, and
+  # testing GraphQL queries.
+  #
+  # Type queries into this side of the screen, and you will see intelligent
+  # typeaheads aware of the current GraphQL type schema and live syntax and
+  # validation errors highlighted within the text.
+  #
+  # GraphQL queries typically start with a "{" character. Lines that start
+  # with a # are ignored.
+  #
+  # Keyboard shortcuts:
+  #
+  #  Prettify Query:  Shift-Ctrl-P (or press the prettify button above)
+  #
+  #     Merge Query:  Shift-Ctrl-M (or press start the merge button above)
+  #
+  #       Run Query:  Ctrl-Enter (or press the play button above)
+  #
+  #   Auto Complete:  Ctrl-Space (or just start typing)
+  #
+
+  
+#  mutation{
+#    sendPushNotification(
+#      input:
+#      {
+#        title: "Low water level",
+#       	message:"The drinking water level in the chicken coop is low. Fill up the water tank",
+#        screenToOpen:HOME
+#      }
+#    )
+#    {
+#      successful,
+#      pushedToNumberOfDevices,
+#      __typename
+#    }
+#  }
+#		
 
 def main():
 	
