557ddd7bc74bc8069d9ae874cc5613a1d2442672
Jo <josmiseth@gmail.com>
2024-06-16 21:48:06 +0200

    add function

--- watertank/sendpushnotification.py
+++ watertank/sendpushnotification.py
@@ -1,19 +1,22 @@
 import requests
 
+def sendpushnotification():
 # Open bearer token file (not checked in to git. Contains only the Tibber bearer token
-with open('bearer.txt','r') as token_file:
-    bearer_token = token_file.read().strip()
+    with open('bearer.secret','r') as token_file:
+        bearer_token = token_file.read().strip()
 
-url = "https://api.tibber.com/v1-beta/gql"
-headers = {"Content-Type": "application/json", "Authorization": "Bearer " + str(bearer_token)}
+    url = "https://api.tibber.com/v1-beta/gql"
+    headers = {"Content-Type": "application/json", "Authorization": "Bearer " + str(bearer_token)}
 
-with open('tibberpush.graphql','r') as query_file:
-    mutation_query = query_file.read().strip()
+    with open('tibberpush.graphql','r') as query_file:
+        mutation_query = query_file.read().strip()
 
-payload = {
-    'query': mutation_query
+    payload = {
+        'query': mutation_query
     }
 
-response = requests.post(url=url, headers=headers, json=payload) 
-print("response status code: ", response.status_code)
-print(response.json())
+    response = requests.post(url=url, headers=headers, json=payload) 
+    print("response status code: ", response.status_code)
+    print(response.json())
+
+sendpushnotification() 
