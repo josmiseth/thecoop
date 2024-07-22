fb3c08ceb08ff990d9654935e01c23cd3b50dda8
Jo <josmiseth@gmail.com>
2024-07-22 16:37:45 +0100

    add limit switch to open_hatch

--- src/open_hatch.py
+++ src/open_hatch.py
@@ -8,13 +8,33 @@ Relay channel     Wire color     RB Port     Function
     4             Yellow         GPIO17      Minus wire hatch up
     3             Green          GPIO05      Plus wire hatch up
     2             Dark blue      GPIO06      Plus wire hatch down
-    1             Blue           GPIO07      Minus wire hatch down
+    1             Blue           GPIO21      Minus wire hatch down
+
+Limit switch
+RB Port
+GPIO16
 
 '''
 
 import time
 from time import sleep
 import RPi.GPIO as GPIO
+
+def limit_reached():
+    
+    # print("Check if limit reached (switch pushed)")
+    
+    state = False
+    try:
+        state = not GPIO.input(16)
+        # print("Pin state: ")
+        # print(state)
+    except:
+        print("Error with pin")
+
+    return state
+
+
 
 def print_text():
     print("Scheduled event")
@@ -33,6 +53,8 @@ def open_hatch():
     relay_minus_up = 17
     relay_plus_up = 5
     time_to_run = 2.0
+    checkloops = 100
+    delta_t = time_to_run/checkloops
     state_file_name = "/tmp/thecoop/hatch_status.txt"
 
 
@@ -41,23 +63,35 @@ def open_hatch():
     GPIO.setup(relay_minus_up, GPIO.OUT)
 
     #Save hatch status running to file
-    set_hatch_status("in_motion", state_file_name)
+ #   set_hatch_status("in_motion", state_file_name)
 
     GPIO.output(relay_plus_up, GPIO.LOW)
     GPIO.output(relay_minus_up, GPIO.LOW)
 
-    time.sleep(time_to_run)
+    
+    while not limit_reached():
+        time.sleep(delta_t)
 
+    print("Limit switch reached")
     GPIO.output(relay_plus_up, GPIO.HIGH)
     GPIO.output(relay_minus_up, GPIO.HIGH)
 
     time.sleep(time_to_run)
     #Save hatch status open to file
     #Save hatch status running to file
-    set_hatch_status("open", state_file_name)
+#    set_hatch_status("open", state_file_name)
 
     print("Open hatch finished")
     return
+
+
+
+GPIO.setmode(GPIO.BCM)     
+pushpin = 16                                        
+GPIO.setup(pushpin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # using the internal Pull up resistor
+
+print(limit_reached())
+
 
 open_hatch()
 print("End")
